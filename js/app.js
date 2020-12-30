7d425f9c729e8d70e04d2ba4387b7c0a7c23020d
Kris Korey Wertman <kkwertman@mail.fhsu.edu>
2020-12-30 17:03:28 +0900

    add conversion formula and basic functionality to section 1: speed app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-// C
+// Accordion section 1: mph to kph converter
+document.getElementById('mph').addEventListener('input', mphToKph);
+document.getElementById('kph').addEventListener('input', kphToMph);
+
+function mphToKph() {
+  let mphRound = mph.value * 1.609344;
+  document.getElementById('kph').value = mphRound.toFixed(2);
+}
+
+function kphToMph() {
+  let kphRound = kph.value / 1.609344;
+  document.getElementById('mph').value = kphRound.toFixed(2);
+}
+
+//Section 1: Need Clear Btn

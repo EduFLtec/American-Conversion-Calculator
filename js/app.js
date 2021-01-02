4688a34fc1a387b275176e4172eadaf69f7e2ad1
Kris Korey Wertman <kkwertman@mail.fhsu.edu>
2021-01-02 18:02:03 +0900

    Change styles and output for UX. Add clear button (functionaly needs add).

--- js/app.js
+++ js/app.js
@@ -4,12 +4,12 @@ document.getElementById('kph').addEventListener('input', kphToMph);
 
 function mphToKph() {
   let mphRound = mph.value * 1.609344;
-  document.getElementById('kph').value = mphRound.toFixed(2);
+  document.getElementById('kph').value = mphRound.toFixed(1);
 }
 
 function kphToMph() {
   let kphRound = kph.value / 1.609344;
-  document.getElementById('mph').value = kphRound.toFixed(2);
+  document.getElementById('mph').value = kphRound.toFixed(1);
 }
 
 //Section 1: Need Clear Btn

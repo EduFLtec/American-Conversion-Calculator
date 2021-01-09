b2034102dd5c65d7a1c3499484607a69699a721b
Kris Korey Wertman <kkwertman@mail.fhsu.edu>
2021-01-09 15:48:10 -0500

    Add Accordion section 2: Fahrenheit to Celsius temp converter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Accordion section 1: mph to kph converter
+// Accordion section 1: mph to kph speed converter
 document.getElementById('mph').addEventListener('input', mphToKph);
 document.getElementById('kph').addEventListener('input', kphToMph);
 
@@ -16,4 +16,24 @@ function kphToMph() {
 document.getElementById('speed-clear-btn').addEventListener('click', () =>{
   document.getElementById('mph').value = '';
   document.getElementById('kph').value = '';
+});
+
+// Accordion section 2: Fahrenheit to Celsius temp converter
+document.getElementById('fahrenheit').addEventListener('input', fToC);
+document.getElementById('celsius').addEventListener('input', cToF);
+
+function fToC() {
+  let fRound = (fahrenheit.value - 32) / 1.8;
+  document.getElementById('celsius').value = fRound.toFixed(1);
+}
+
+function cToF() {
+  let cRound = (celsius.value * 1.8) + 32;
+  document.getElementById('fahrenheit').value = cRound.toFixed(1);
+}
+
+//Section 2: Clear Button
+document.getElementById('temp-clear-btn').addEventListener('click', () =>{
+  document.getElementById('fahrenheit').value = '';
+  document.getElementById('celsius').value = '';
 });
